1eca1e80fc2aba6eb10ed7c2575bae651990004e
phoenixx-png <jojoeydenver@gmail.com>
2026-05-12 19:53:19 +0200

    Removed all code files in the '_zip_scrolls' directory

--- _zip_scrolls/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import ChineseScrollsSection from "./components/ChineseScrollsSection";
-
-export default function App() {
-  return (
-    <div className="size-full">
-      <ChineseScrollsSection />
-    </div>
-  );
-}
